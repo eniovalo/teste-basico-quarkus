e6ad9909274a592ca33c8bd4903f3ddd47a14e5c
Enio <enio9999@yahoo.com.br>
2022-10-03 15:54:03 -0300

    Obtendo data atual

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,8 @@
 package org.acme;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,10 +11,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        final Integer texto = "Olá Quarkus";
-        return texto;
+        final String texto = "Olá Quarkus";
+        final String dataAtualFormatada = dateTimeFormatter.format(LocalDate.now());
+
+        return texto + " - " + dataAtualFormatada;
     }
 }
--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -6,16 +6,24 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @QuarkusTest
 public class GreetingResourceTest {
 
+    final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     @Test
     public void testHelloEndpoint() {
+        final String texto = "Olá Quarkus";
+        final String dataAtualFormatada = dateTimeFormatter.format(LocalDate.now());
+
         given()
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Olá Quarkus"));
+             .body(is(texto + " - " + dataAtualFormatada));
     }
 
 }
