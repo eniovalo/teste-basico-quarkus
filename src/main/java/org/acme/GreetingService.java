c78374fd4136d1a716c79fc7dc132d2267d1a545
eniovalo <enio9999@yahoo.com.br>
2023-07-23 11:59:46 -0300

    exercicio

--- src/main/java/org/acme/GreetingService.java
+++ src/main/java/org/acme/GreetingService.java
@@ -8,12 +8,11 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class GreetingService {
     public LocalDate getData(final String dataTexto) {
-        var df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return LocalDate.parse(dataTexto, df);
+        return dataTexto;
     }
 
     public int somar(final int numero1, final int numero2) {
-        return numero1 + numero2;
+        return numero1 - numero2;
     }
 
     public double multiplicar(final double numero1, final double numero2) {
